6f85bcf7955084b5494e9c38d1ee5ec143f3f6f0
Alex <take-me-home@kakao.com>
2022-11-09 02:20:52 +0900

    chore: remove unimportant dependencies.

--- app/src/main/java/com/github/thepiemonster/hidemocklocation/AboutActivity.java
+++ app/src/main/java/com/github/thepiemonster/hidemocklocation/AboutActivity.java
@@ -1,60 +1,14 @@
 package com.github.thepiemonster.hidemocklocation;
 
-import android.annotation.SuppressLint;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-
-import com.drakeet.about.AbsAboutActivity;
-import com.drakeet.about.Card;
-import com.drakeet.about.Category;
-import com.drakeet.about.Contributor;
-import com.drakeet.about.License;
-import com.drakeet.about.OnRecommendationClickedListener;
-import com.drakeet.about.Recommendation;
-
-import java.util.List;
-
-
-public class AboutActivity extends AbsAboutActivity
-        implements OnRecommendationClickedListener {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setOnRecommendationClickedListener(this);
-    }
-
-    @SuppressLint("SetTextI18n")
-    @Override
-    protected void onCreateHeader(@NonNull ImageView icon, @NonNull TextView slogan, @NonNull TextView version) {
-        icon.setImageResource(R.mipmap.ic_launcher);
-        slogan.setText(R.string.about_slogan);
-        version.setText("v" + BuildConfig.VERSION_NAME);
-    }
-
-    @Override
-    protected void onItemsCreated(@NonNull List<Object> items) {
-        items.add(new Category(getString(R.string.about_introduction_title)));
-        items.add(new Card(getString(R.string.about_introduction_content)));
-
-        items.add(new Category(getString(R.string.about_developer_title)));
-        items.add(new Contributor(R.drawable.avatar_pie, getString(R.string.about_developer_name_pie), getString(R.string.about_developer_description_pie), getString(R.string.about_developer_url_pie)));
-
-        items.add(new Category(getString(R.string.about_opensource_title)));
-        items.add(new License("About Page", "drakeet", License.APACHE_2, "https://github.com/drakeet/about-page"));
-
-        items.add(new Category(getString(R.string.about_legal_title)));
-        items.add(new Card(getString(R.string.about_legal_content)));
-    }
-
-    @Override
-    public boolean onRecommendationClicked(@NonNull View itemView, @NonNull Recommendation recommendation) {
-        Toast.makeText(this, "" + recommendation.appName, Toast.LENGTH_SHORT).show();
-        return false;
+        setContentView(R.layout.activity_about);
     }
 }
--- app/src/main/java/com/github/thepiemonster/hidemocklocation/Common.java
+++ app/src/main/java/com/github/thepiemonster/hidemocklocation/Common.java
@@ -1,16 +1,9 @@
 package com.github.thepiemonster.hidemocklocation;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
-
 public class Common {
 
     public static final String PACKAGE_NAME = Common.class.getPackage().getName();
     public static final String ACTIVITY_NAME = PACKAGE_NAME + ".MainActivity";
 
     public static final String GMS_MOCK_KEY = "mockLocation"; // FusedLocationProviderApi.KEY_MOCK_LOCATION
-
-    // Processes/Packages that should always see true 'Allow mock locations' value
-    public static final HashSet<String> SYSTEM_WHITELIST = new HashSet<>(Arrays.asList("com.android.settings", "com.sec.android.providers.security"));
 }
